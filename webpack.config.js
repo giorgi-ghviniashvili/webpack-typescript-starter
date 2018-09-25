890d2ea4c488da9c1956492341d7bc305b937ad9
giorgi-ghviniashvili <mr.g.ghv@gmail.com>
2018-09-25 14:51:36 +0400

    initial

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/app'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+      contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname,'dist')
+  }
+}
